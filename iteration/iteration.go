33a480cf1005cb1545f54d17e5c1ab752aa63b21
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 16:54:08 -0300

    feat: ensure Iteration returns an string with repetitions of a character

---
+++ iteration/iteration.go
@@ -0,0 +1,9 @@
+package iteration
+
+func Iteration(character string) string {
+	var repetitions string
+	for i := 0; i < 5; i++ {
+		repetitions = repetitions + character
+	}
+	return repetitions
+}
