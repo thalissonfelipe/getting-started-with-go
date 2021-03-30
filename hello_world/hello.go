55531253e57a248c6697d8f8de38c91e52cf462d
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 16:05:16 -0300

    add default message when an empty string is provided

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const prefixHello = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return prefixHello + name
 }
 
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Felipe")
-	expect := "Hello, Felipe"
-
-	if result != expect {
-		t.Errorf("result '%s', expect '%s'", result, expect)
+	verifyCorrectMessage := func(t *testing.T, result, expect string) {
+		t.Helper()
+		if result != expect {
+			t.Errorf("result '%s', expect '%s'", result, expect)
+		}
 	}
+
+	t.Run("should say hello to people", func(t *testing.T) {
+		result := Hello("Felipe")
+		expect := "Hello, Felipe"
+		verifyCorrectMessage(t, result, expect)
+	})
+
+	t.Run("should say 'Hello, world' when an empty string is provided", func(t *testing.T) {
+		result := Hello("")
+		expect := "Hello, world"
+		verifyCorrectMessage(t, result, expect)
+	})
 }
