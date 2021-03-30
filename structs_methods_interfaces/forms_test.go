4d68220120cac5b445a0fa1a4f12d76709a0e613
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 18:16:03 -0300

    refactor: add table driven tests

--- structs_methods_interfaces/forms_test.go
+++ structs_methods_interfaces/forms_test.go
@@ -13,22 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	verifyArea := func(t *testing.T, form Form, expected float64) {
-		t.Helper()
-		result := form.Area()
-
-		if result != expected {
-			t.Errorf("result %.2f expected %.2f", result, expected)
-		}
+	testsArea := []struct {
+		form     Form
+		expected float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("should return an area when the object is a rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		verifyArea(t, rectangle, 72.0)
-	})
-
-	t.Run("should return an area when the object is a circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		verifyArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range testsArea {
+		result := tt.form.Area()
+		if result != tt.expected {
+			t.Errorf("result %.2f, expected %.2f", result, tt.expected)
+		}
+	}
 }
