605bf74a39020d57dc8525030ed2645e169ecf73
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 17:03:36 -0300

    test: add number parameter to Iteration function

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,10 +3,22 @@ package iteration
 import "testing"
 
 func TestIteration(t *testing.T) {
-	result := Iteration("a")
-	expected := "aaaaa"
-
-	if result != expected {
-		t.Errorf("result '%s', expected '%s'", result, expected)
+	verifyCorrectOutput := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("should return a string with repetitions", func(t *testing.T) {
+		result := Iteration("a", 5)
+		expected := "aaaaa"
+		verifyCorrectOutput(t, result, expected)
+	})
+
+	t.Run("should return a string with N repetitions", func(t *testing.T) {
+		result := Iteration("a", 3)
+		expected := "aaa"
+		verifyCorrectOutput(t, result, expected)
+	})
 }
