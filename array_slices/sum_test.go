d782bb20fdf2282dcc98071d601ef288c4bd9d3e
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 17:29:25 -0300

    refactor: use slice instead an array

--- array_slices/sum_test.go
+++ array_slices/sum_test.go
@@ -3,11 +3,17 @@ package array_slices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5} // [...]int{1, 2, 3, 4, 5}
-	result := Sum(numbers)
-	expected := 15
-
-	if result != expected {
-		t.Errorf("result %d, expected %d, given %v", result, expected, numbers)
+	verifyCorrectOutput := func(t *testing.T, result int, expected int, numbers []int) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result %d, expected %d, given %v", result, expected, numbers)
+		}
 	}
+
+	t.Run("should return a sum of an array with any numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		result := Sum(numbers)
+		expected := 6
+		verifyCorrectOutput(t, result, expected, numbers)
+	})
 }
