7bc8b6364a3ae8bbd491e4e5b30e35c7ed3fd40a
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-30 18:01:00 -0300

    refactor: create Rectangle struct to hold width and height properties

--- structs_methods_interfaces/forms_test.go
+++ structs_methods_interfaces/forms_test.go
@@ -3,7 +3,8 @@ package structs_methods_interfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	result := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	result := Perimeter(rectangle)
 	expected := 40.0
 
 	if result != expected {
@@ -12,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	result := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	result := Area(rectangle)
 	expected := 72.0
 
 	if result != expected {
