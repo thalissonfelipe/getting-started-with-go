93c8b73403a32be89b6a80a77d1cac9775481314
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-31 11:54:32 -0300

    test: add Widhdraw method to Wallet type

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -3,13 +3,27 @@ package pointers_errors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	result := wallet.Balance()
-	expected := Bitcoin(10)
+		result := wallet.Balance()
+		expected := Bitcoin(10)
 
-	if result != expected {
-		t.Errorf("result %s, expected %s", result, expected)
-	}
+		if result != expected {
+			t.Errorf("result %s, expected %s", result, expected)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		result := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if result != expected {
+			t.Errorf("result %s, expected %s", result, expected)
+		}
+	})
 }
