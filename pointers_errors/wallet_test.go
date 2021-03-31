752be216894a1cb8cc2a820f2f2916f5891d553e
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-31 11:44:20 -0300

    test: add Wallet struct and Deposit and Balance methods

---
+++ pointers_errors/wallet_test.go
@@ -0,0 +1,15 @@
+package pointers_errors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	result := wallet.Balance()
+	expected := 10
+
+	if result != expected {
+		t.Errorf("result %d, expected %d", result, expected)
+	}
+}
