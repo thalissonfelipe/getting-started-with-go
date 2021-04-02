a43ce2f85f6a5ba513909014edc2f169b337e299
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-02 11:49:40 -0300

    feat: ensure Search returns an error when the word does not exist

--- maps/dict.go
+++ maps/dict.go
@@ -1,7 +1,14 @@
 package maps
 
+import "errors"
+
 type Dict map[string]string
 
-func (d Dict) Search(word string) string {
-	return d[word]
+func (d Dict) Search(word string) (string, error) {
+	value, exists := d[word]
+	if !exists {
+		return "", errors.New("could not find the word you are looking for")
+	}
+
+	return value, nil
 }
