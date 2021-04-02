245efe45eb6439f50e9e8aacda0b6c583c81e12b
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-02 11:49:48 -0300

    test: ensure Search returns an error when the word does not exist

--- maps/dict_test.go
+++ maps/dict_test.go
@@ -5,10 +5,20 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dict := Dict{"test": "this is just a test"}
 
-	result := dict.Search("test")
-	expected := "this is just a test"
+	t.Run("search a known word", func(t *testing.T) {
+		result, _ := dict.Search("test")
+		expected := "this is just a test"
 
-	compareStrings(t, result, expected)
+		compareStrings(t, result, expected)
+	})
+
+	t.Run("search an unknown word", func(t *testing.T) {
+		_, err := dict.Search("unknown")
+
+		if err == nil {
+			t.Fatal("an error is expected to be obtained")
+		}
+	})
 }
 
 func compareStrings(t *testing.T, result, expected string) {
