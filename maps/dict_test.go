6f531987234697ce5a96c21456332c68291176ce
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-02 11:40:20 -0300

    refactor: create helper function to compare strings

--- maps/dict_test.go
+++ maps/dict_test.go
@@ -7,6 +7,12 @@ func TestSearch(t *testing.T) {
 	result := Search(dict, "test")
 	expected := "this is just a test"
 
+	compareStrings(t, result, expected)
+}
+
+func compareStrings(t *testing.T, result, expected string) {
+	t.Helper()
+
 	if result != expected {
 		t.Errorf("result '%s', expected '%s', given '%s'", result, expected, "test")
 	}
