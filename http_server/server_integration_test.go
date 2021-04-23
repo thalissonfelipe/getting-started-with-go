c99e98729cda16a9576e6f00b25491bcfada7794
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-23 20:57:58 -0300

    ensure InMemoryPlayerStore returns players on get /league request

--- http_server/server_integration_test.go
+++ http_server/server_integration_test.go
@@ -15,9 +15,26 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
 
-	assertResponseBody(t, response.Body.String(), "3")
+		server.ServeHTTP(response, newGetScoreRequest(player))
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "3")
+	})
+
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, newLeagueRequest())
+
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{"Pepper", 3},
+		}
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertLeague(t, got, want)
+	})
 }
