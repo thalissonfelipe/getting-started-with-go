0c8b8473b41996f83a0f6e6f2e0e288e02fe2a1d
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-22 21:15:52 -0300

    refactor main.go to use in memory struct with maps

--- main.go
+++ main.go
@@ -7,15 +7,7 @@ import (
 	hs "github.com/thalissonfelipe/learn-go/http_server"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
 func main() {
-	server := &hs.PlayerServer{&InMemoryPlayerStore{}}
+	server := &hs.PlayerServer{hs.NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
