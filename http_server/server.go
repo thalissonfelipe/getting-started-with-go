887be4daf798d5dc6b56d0bfec339805734ce948
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-22 21:07:20 -0300

    ensure the correct player has the score stored

--- http_server/server.go
+++ http_server/server.go
@@ -19,7 +19,7 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w)
+		p.processWin(w, r)
 	case http.MethodGet:
 		p.showScore(w, r)
 	}
@@ -38,7 +38,8 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
-	p.Store.RecordWin("Bob")
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
